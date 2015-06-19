a4fbcf41f79513266837e5882056db8dba6165a8
nameless <a-arsenij@yandex.ru>
2015-06-19 16:55:16 +0500

    API for editor

---
+++ src/Shootan/UI/EDITORInterface/AWTInterface/EditorUI.java
@@ -0,0 +1,46 @@
+package Shootan.UI.EDITORInterface.AWTInterface;
+
+import javax.swing.*;
+
+public class EditorUI extends JFrame {
+    private JPanel contentPane;
+
+    private UICanvas uic;
+
+    public EditorUI() {
+
+        try {
+            UIManager.setLookAndFeel(com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel.class.getCanonicalName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        setContentPane(contentPane);
+
+        uic = new UICanvas();
+
+        contentPane.add(uic);
+
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(1100, 700);
+
+        setResizable(true);
+
+        setVisible(true);
+
+
+        uic.start();
+    }
+
+
+    public static void main(String[] args) {
+        new EditorUI();
+    }
+}
---
+++ src/Shootan/UI/EDITORInterface/AWTInterface/UICanvas.java
@@ -0,0 +1,124 @@
+package Shootan.UI.EDITORInterface.AWTInterface;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferStrategy;
+
+public class UICanvas extends Canvas {
+
+    public UICanvas() {
+        super();
+
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+
+        addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+            }
+        });
+
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+        addMouseWheelListener(e -> {
+        });
+
+        addComponentListener(new ComponentListener() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+
+            }
+
+            @Override
+            public void componentMoved(ComponentEvent e) {
+
+            }
+
+            @Override
+            public void componentShown(ComponentEvent e) {
+
+            }
+
+            @Override
+            public void componentHidden(ComponentEvent e) {
+
+            }
+        });
+    }
+
+    public void start() {
+        createBufferStrategy(3);
+        bs = getBufferStrategy();
+        g2 = (Graphics2D) bs.getDrawGraphics();
+        new Timer(20, e -> {
+            update();
+            draw();
+        }).start();
+    }
+
+    private BufferStrategy bs;
+    private Graphics2D g2;
+
+    private void update() {
+
+
+
+    }
+
+    private void draw() {
+        g2.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        g2.setColor(new Color(0, 0, 0));
+        g2.fillRect(0, 0, getWidth(), getHeight());
+
+        /*
+        YOUR FUKKEN RENDER
+         */
+
+        bs.show();
+    }
+}
