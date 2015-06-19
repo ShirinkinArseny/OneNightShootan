20902b6165d6b13c96c262d517b284aa0a17de81
god <discantgrig@mail.ru>
2015-06-19 22:33:14 +0500

    Added block texture, now can drag mouse and DO BLOCKS

---
+++ src/Shootan/UI/EDITORInterface/Block.java
@@ -0,0 +1,33 @@
+package Shootan.UI.EDITORInterface;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Block {
+    int x,y,size=100;
+    BufferedImage bi;
+
+
+
+    public Block(int x, int y){
+        this.x=x;
+        this.y=y;
+        try {
+            bi= ImageIO.read(new File("content/brick.png"));
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    protected void draw(Graphics2D g2){
+        g2.drawImage(bi,x,y,x+size,y+size,0,0,128,128,null);
+    }
+
+    protected boolean cross(int x, int y){
+        return ((x<=this.x+size)&&(x>=this.x-size)&&(y<=this.y+size)&&(y>=this.y-size));
+    }
+
+}
